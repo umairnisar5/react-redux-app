5c09322e41de3523ee28093e03d38abea5cf8c76
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-03-10 18:01:41 +0500

    create a action.js

---
+++ src/Components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+function Home () {
+    return (
+        <div> well come home  components</div>
+    )
+}
+
+export default Home ;
---
+++ src/container/HomeContainer.js
@@ -0,0 +1,5 @@
+import Home from "../Components/home";
+
+
+
+export default Home 
