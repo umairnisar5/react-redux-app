4418b72ec21f1e995708c425455bcfc2e432df5c
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2021-03-15 17:00:22 +0500

    create a card nd update css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './service/reducers/index'
+const store=createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
